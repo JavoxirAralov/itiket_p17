9d6f92b8008b5729a34cce3b21cf31479d3fb174
your username here <your email here>
2024-05-14 19:20:54 +0500

    fixture changes

--- Makefile
+++ Makefile
@@ -1,21 +1,32 @@
 loaddata:
+	python manage.py loaddata country
+	python manage.py loaddata city
 	python manage.py loaddata category
+	python manage.py loaddata categorytranslation
 	python manage.py loaddata location
+	python manage.py loaddata locationtranslation
 	python manage.py loaddata venues
+	python manage.py loaddata venuestranslation
 	python manage.py loaddata event
+	python manage.py loaddata eventtranslation
 	python manage.py loaddata promocode
-	python manage.py loaddata like
+	python manage.py createsuperuser
+	python manage.py loaddata favorite
 	python manage.py loaddata basket
 	python manage.py loaddata courier
 	python manage.py loaddata promotion
 	python manage.py loaddata order
 	python manage.py loaddata session
+	python manage.py loaddata sessiontranslation
 
 
 
+make product :
+	python manage.py loaddata product
+	python manage.py loaddata producttranslation
 
 
-migrate:
+mig:
 	python manage.py makemigrations
 	python manage.py migrate
 docker:
@@ -25,8 +36,6 @@ docker:
 
 
 admin:
-	python manage.py loaddata country
-	python manage.py loaddata city
 	python3 manage.py createsuperuser
 
 run:
--- apps/models.py
+++ apps/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import Model, DateTimeField, CharField, EmailField, ForeignKey, CASCADE, IntegerField, \
-    BooleanField, ManyToManyField, PositiveIntegerField, DecimalField
+    BooleanField, ManyToManyField, PositiveIntegerField, DecimalField, SlugField
 from django_ckeditor_5.fields import CKEditor5Field
 from django_resized import ResizedImageField
 from parler.models import TranslatableModel, TranslatedFields
@@ -40,19 +40,13 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    def __str__(self):
-        return self.username
-
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
 
 
-class Country(TranslatableModel):
-    translations = TranslatedFields(
-        name=CharField(verbose_name=_('name'), max_length=150, unique=True)
-    )
-
+class Country(Model):
+    name = CharField(max_length=150, unique=True)
     code = CharField(max_length=70)
 
     def __str__(self):
@@ -63,10 +57,8 @@ class Country(TranslatableModel):
         verbose_name_plural = _('Countries')
 
 
-class City(TranslatableModel):
-    translations = TranslatedFields(
-        name=CharField(verbose_name=_('name'), max_length=255)
-    )
+class City(Model):
+    name = CharField(max_length=255)
     country = ForeignKey('Country', CASCADE)
 
     def __str__(self):
@@ -80,30 +72,27 @@ class City(TranslatableModel):
 class Event(StartEndBaseModel, TranslatableModel):
     translations = TranslatedFields(
         description=CKEditor5Field(verbose_name=_('description'), blank=True, null=True, config_name='extends'),
-        title=CharField(verbose_name=_('title'), max_length=70),
+        title=CharField(verbose_name=_('title'), max_length=70)
     )
     image = ResizedImageField(size=[200, 200], crop=['middle', 'center'], upload_to='event',
                               default='event/event_default/default.jpg')
     price = PositiveIntegerField(default=0)
     city = ForeignKey('City', CASCADE)
     category = ForeignKey('Category', CASCADE)
-    slug = CharField(max_length=100)  # add slug  in  fixture
+    slug = SlugField(max_length=100, editable=False)  # add slug  in  fixture
+    venue = ForeignKey('apps.Venue', CASCADE)
 
     def __dir__(self):
-        return self.translations
+        return self.title
 
     class Meta:
         verbose_name = _('Event')
         verbose_name_plural = _('Events')
 
 
-class Promotion(TranslatableModel):
-    translations = TranslatedFields(
-        name=CharField(verbose_name=_('name'), max_length=255),
-
-    )
-
-    event = ManyToManyField('Event', blank=True, related_name='promotions')
+class Promotion(Model):
+    name = CharField(max_length=255)
+    event = ManyToManyField('Event', blank=True)
 
     def __str__(self):
         return self.name
@@ -115,13 +104,13 @@ class Promotion(TranslatableModel):
 
 class Session(StartEndBaseModel, TranslatableModel):
     translations = TranslatedFields(
-        name=CharField(verbose_name=_('name'), max_length=100),
+        name=CharField(verbose_name=_('name'), max_length=100)
 
     )
 
     price = PositiveIntegerField(default=0)
     event = ForeignKey('Event', CASCADE)
-    order = ForeignKey('Order', CASCADE)
+    order = ForeignKey('apps.Order', CASCADE)
 
     def __str__(self):
         return self.name
@@ -133,8 +122,7 @@ class Session(StartEndBaseModel, TranslatableModel):
 
 class Location(TranslatableModel):
     translations = TranslatedFields(
-        description=CKEditor5Field(verbose_name=_('description'), blank=True, null=True, config_name='extends'),
-
+        description=CKEditor5Field(verbose_name=_('description'), blank=True, null=True, config_name='extends')
     )
     index = IntegerField()
     city = ForeignKey('City', CASCADE)
@@ -142,7 +130,7 @@ class Location(TranslatableModel):
     latitude = DecimalField(max_digits=9, decimal_places=6)
     longitude = DecimalField(max_digits=9, decimal_places=6)
     is_deleted = BooleanField(default=False)
-    order = ManyToManyField('Order', related_name='locations')
+    order = ManyToManyField('Order', related_name='locations', blank=True)
 
     def __str__(self):
         return str(self.index)
@@ -152,13 +140,9 @@ class Location(TranslatableModel):
         verbose_name_plural = _('Locations')
 
 
-class Order(TranslatableModel):
-    translations = TranslatedFields(
-        firstname=CharField(verbose_name=_('firstname'), max_length=70),
-        lastname=CharField(verbose_name=_('lastname'), max_length=70),
-
-    )
-
+class Order(Model):
+    first_name = CharField(max_length=70)
+    last_name = CharField(max_length=70)
     phone = CharField(max_length=25, validators=[phone_regex], help_text='+9989***')
     email = EmailField(unique=True)
     promo = ForeignKey('PromoCode', CASCADE)
@@ -167,7 +151,7 @@ class Order(TranslatableModel):
     courier = ForeignKey('Courier', CASCADE)
 
     def __str__(self):
-        return self.firstname
+        return self.first_name
 
     class Meta:
         verbose_name = _('Order')
@@ -178,7 +162,7 @@ class Category(TranslatableModel):
     translations = TranslatedFields(
         name=CharField(verbose_name=_('name'), max_length=255),
     )
-    slug = CharField(max_length=100)
+    slug = SlugField(max_length=100, editable=False)
 
     def __str__(self):
         return self.name
@@ -198,19 +182,17 @@ class Basket(Model):
         verbose_name_plural = _('Baskets')
 
 
-class Like(TranslatableModel):
-    translations = TranslatedFields(
-        name=ForeignKey('apps.User', verbose_name=_('name'), on_delete=models.CASCADE),
-    )
-    like = BooleanField(default=False)
+class Favorite(Model):
+    favorite = CharField(max_length=255, blank=True)
+    user = ForeignKey('User', CASCADE)
     event = ForeignKey('Event', CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.favorite
 
     class Meta:
-        verbose_name = _('Like')
-        verbose_name_plural = _('Likes')
+        verbose_name = _('Favorite')
+        verbose_name_plural = _('Favorites')
 
 
 class Venue(TranslatableModel):
@@ -219,15 +201,15 @@ class Venue(TranslatableModel):
         description=CKEditor5Field(verbose_name=_('description'), blank=True, null=True, config_name='extends'),
         address=CharField(verbose_name=_('address'), max_length=100, blank=True)
     )
-    banner = ResizedImageField(size=[1000, 320], crop=['middle', 'center'], upload_to='venues_banner')
+    banner = ResizedImageField(size=[1000, 320], crop=['middle', 'center'], upload_to='venues_banner',
+                               default='venues/venues_default/default.jpg')
     image = ResizedImageField(size=[200, 200], crop=['middle', 'center'], upload_to='venues',
                               default='venues/venues_default/default.jpg')
-    # location = ForeignKey('apps.Location', CASCADE)  # TODO: check
-    slug = CharField(max_length=100)
-    lang = CharField(max_length=10)
-    lat = CharField(max_length=10)
+    slug = SlugField(max_length=100, editable=False)
+
+    latitude = DecimalField(max_digits=9, decimal_places=6)
+    longitude = DecimalField(max_digits=9, decimal_places=6)
     phone = CharField(max_length=25, validators=[phone_regex], help_text='+9989***')
-    event = ForeignKey('apps.Event', CASCADE)  # it was chnaged the event and venu ForeginKey, change fixiture also
 
     def __str__(self):
         return self.title
@@ -237,25 +219,19 @@ class Venue(TranslatableModel):
         verbose_name_plural = _('Venues')
 
 
-class PromoCode(StartEndBaseModel, TranslatableModel):
-    translations = TranslatedFields(
-        promo=CharField(verbose_name=_('promo'), max_length=255)  # So Promo cod will be always in English laltar
-    )
-
-    def __str__(self):
-        return self.promo
+class PromoCode(StartEndBaseModel):
+    name = CharField(verbose_name=_('promo'), max_length=255)
 
     class Meta:
         verbose_name = _('PromoCode')
         verbose_name_plural = _('PromoCodes')
 
 
-class Courier(TranslatableModel):
-    translations = TranslatedFields(
-        title=CharField(verbose_name=_('title'), max_length=70),
-        description=CKEditor5Field(verbose_name=_('description'), blank=True, null=True, config_name='extends'),
-        street=CharField(verbose_name=_('street'), max_length=100)
-    )
+class Courier(Model):
+    title = CharField(max_length=70)
+    description = CKEditor5Field(blank=True, null=True, config_name='extends')
+    street = CharField(verbose_name=_('street'), max_length=100)
+
     building = CharField(max_length=100)
     house_number = CharField(max_length=100)  # House number might be  -  74A
     index = CharField(max_length=100)  # Index number might  be  - AB12345
@@ -268,3 +244,26 @@ class Courier(TranslatableModel):
     class Meta:
         verbose_name = _('Courier')
         verbose_name_plural = _('Couriers')
+
+
+class Product(Model):
+    name = CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = ('Courier')
+        verbose_name_plural = ('Couriers')
+
+
+class Info(Model):
+    name = CharField(max_length=255)
+    product = ManyToManyField('Product', related_name='pruduct')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = ('Courier')
+        verbose_name_plural = ('Couriers')
